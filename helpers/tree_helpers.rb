e51a4c0abbe26919edb373467255030ca9a734f8
Joel Moss <joel@developwithstyle.com>
2012-11-20 18:10:14 +0000

    tree no longer relies on a yaml file, but if one exists, it will be used

--- helpers/tree_helpers.rb
+++ helpers/tree_helpers.rb
@@ -1,96 +1,62 @@
 module TreeHelpers
 
-  def documentation_tree
-    dir = 'source/developers/documentation'
-    tree_file = "#{root}/tree.yml"
+  def tree_for(dir)
+    content_tag :ol do
 
-    # Load the tree from the tree.yml file if it exists.
-    if File.exists?(tree_file)
-      tree = YAML::load(File.read(tree_file))
-
-    # else we scan the directory contents
-    else
-      content_tag :ol do
-
-        Dir.new(dir).each do |x|
-          next if x == '.' || x == '..' || x.start_with?('index.')
-
-          file = "#{dir}/#{x}"
-          if Dir.exists?(file)
-            resource = resource_for("#{file}/index.html.markdown")
-
-            content_tag :li do
-              content_tag :div, :class => "nofade" do
-                content_tag :span do
-                  "expand"
-                end
-                content_tag :a, :href => resource.url do
-                  resource.data.title
-                end
-              end
-            end
-
-          else
-            resource = resource_for(file)
-
-            content_tag :li, :class => "nofade" do
-              content_tag :a, :href => resource.url, :class => current_page?(resource.url) ? 'active' : '' do
-                resource.data.title
-              end
-            end
-          end
+      # Load the tree from the tree.yml file if it exists.
+      if File.exists?(tree_file = "#{dir}/tree.yml")
+        YAML::load(File.read(tree_file)).each do |item|
+          build_tree "#{dir}/#{item['url']}"
         end
 
+      # else we scan the directory contents
+      else
+        Dir.new(dir).each do |x|
+          next if x == '.' || x == '..' || x.start_with?('index.')
+          build_tree "#{dir}/#{x}"
+        end
       end
-    end
 
-    # content_tag :aside, :class => "tree" do
-    #   generate_tree_for data['tree']
-    # end
+    end
   end
 
 
   private
 
-    def tree_for(file)
-      content_tag :ol do
+    def build_tree(file)
+      if Dir.exists?(file)
+        build_directory file
+      else
+        resource = resource_for(file)
 
+        content_tag :li, :class => "nofade" do
+          content_tag :a, :href => resource.url, :class => current_page?(resource.url) ? 'active' : '' do
+            resource.data.title
+          end
+        end
+      end
+    end
+
+    def build_directory(file)
+      resource = resource_for("#{file}/index.html.markdown")
+
+      content_tag :li do
+        content_tag :div, :class => "nofade" do
+          content_tag :span do
+            "expand"
+          end
+          content_tag :a, :href => resource.url do
+            resource.data.title
+          end
+        end
+
+        tree_for file
       end
     end
 
     def resource_for(file)
       path = sitemap.file_to_path(file)
       resource = sitemap.find_resource_by_path(path)
-    end
-
-    def generate_tree_for(data)
-      content_tag :ol do
-        data.each do |ele|
-          if ele['tree']
-            content_tag :li do
-              content_tag :div, :class => "nofade" do
-                content_tag :span do
-                  "expand"
-                end
-                content_tag :a, :href => ele['url'] do
-                  ele['title']
-                end
-              end
-              generate_tree_for ele['tree']
-            end
-          else
-            content_tag :li, :class => "nofade" do
-              content_tag :a, :href => ele['url'], :class => current_page?(ele['url']) ? 'active' : '' do
-                ele['title']
-              end
-            end
-          end
-        end
-      end
-    end
-
-    def random_string
-      rand(36**16).to_s(36)
     end
 
     def current_page?(path)
